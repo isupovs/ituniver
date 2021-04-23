67b39a314ab8966e8bad1662b8fcb8904aa5494c
Sergey Isupov <SIsupov@ita-labs.ru>
2021-04-23 13:43:57 +0300

    added check if start is a natural number

--- first/square_generator.go
+++ first/square_generator.go
@@ -4,6 +4,9 @@ func SolutionSquareGenerator(start int, n int) []int {
 	if n < 1 {
 		panic("n must be greater than 0")
 	}
+	if start < 1 {
+		panic("start must be a natural number")
+	}
 	res := make([]int, n)
 	for i := 0; i < n; i++ {
 		res[i] = start * start
--- first/square_generator_test.go
+++ first/square_generator_test.go
@@ -6,7 +6,14 @@ import (
 )
 
 func TestSolutionSquareGenerator(t *testing.T) {
-	t.Run("panic if less than 1", func(t *testing.T) {
+	t.Run("panic if n is less than 1", func(t *testing.T) {
+		defer func() { recover() }()
+		SolutionSquareGenerator(10, 0)
+
+		t.Error("should have panicked")
+	})
+
+	t.Run("panic if 'start' is not a natural number", func(t *testing.T) {
 		defer func() { recover() }()
 		SolutionSquareGenerator(10, 0)
 
@@ -22,9 +29,9 @@ func TestSolutionSquareGenerator(t *testing.T) {
 	})
 
 	t.Run("collection of any size", func(t *testing.T) {
-		expexted := []int{25, 16, 9, 4, 1, 0, 1, 4, 9, 16}
+		expexted := []int{1, 4, 9, 16, 25, 36, 49, 64, 81, 100}
 
-		got := SolutionSquareGenerator(-5, 10)
+		got := SolutionSquareGenerator(1, 10)
 		if !reflect.DeepEqual(expexted, got) {
 			t.Errorf("wrong result. expected: %v, got: %v", expexted, got)
 		}
